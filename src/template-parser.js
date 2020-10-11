e62c12c9da31913998afe612cfd315fa01debcd3
Lars Jacobsson <lars.jacobsson@mathem.se>
2020-10-11 22:38:56 +0200

    yaml dump fix

--- src/template-parser.js
+++ src/template-parser.js
@@ -133,11 +133,20 @@ async function extractSamDefinition() {
 
   const extractEvent = await inputUtil.selectFrom(
     events,
-    "Select SAM event to extract"
+    "Select SAM event to extract",
+    true
   );
 
-  delete template.Resources[extractEvent.function].Properties.Events[extractEvent.event];
+  delete template.Resources[extractEvent.function].Properties.Events[
+    extractEvent.event
+  ];
 
+  if (
+    Object.keys(template.Resources[extractEvent.function].Properties.Events)
+      .length === 0
+  ) {
+    delete template.Resources[extractEvent.function].Properties.Events;
+  }
 
   let suggestion = `${extractEvent.event}Rule`;
   if (template.Resources[suggestion]) {
@@ -151,29 +160,35 @@ async function extractSamDefinition() {
       EventBusName: extractEvent.config.EventBusName || "default",
       EventPattern: extractEvent.config.Pattern,
       State: "ENABLED",
-      Targets: [{ 
-        Arn: { "Fn::GetAtt": [extractEvent.function, "Arn"] },
-        Id: suggestion,
-        Input: extractEvent.config.Input,
-        InputPath: extractEvent.config.InputPath
-      }],
+      Targets: [
+        {
+          Arn: { "Fn::GetAtt": [extractEvent.function, "Arn"] },
+          Id: suggestion,
+        },
+      ],
     },
   };
+
+  if (extractEvent.config.Input) {
+    template.Resources[resourceName].Properties.Targets[0].Input =
+      extractEvent.config.Input;
+  }
+  if (extractEvent.config.InputPath) {
+    template.Resources[resourceName].Properties.Targets[0].InputPath =
+      extractEvent.config.InputPath;
+  }
   template.Resources[`${resourceName}Permission`] = {
     Type: "AWS::Lambda::Permission",
-      Properties: {
-        FunctionName: {
-          Ref: extractEvent.function
-        },
-        Action: "lambda:InvokeFunction",
-        Principal: "events.amazonaws.com",
-        SourceArn: {
-          "Fn::GetAtt": [
-            suggestion,
-            "Arn"
-          ]
-        }
-      }
+    Properties: {
+      FunctionName: {
+        Ref: extractEvent.function,
+      },
+      Action: "lambda:InvokeFunction",
+      Principal: "events.amazonaws.com",
+      SourceArn: {
+        "Fn::GetAtt": [suggestion, "Arn"],
+      },
+    },
   };
 
   saveTemplate();
