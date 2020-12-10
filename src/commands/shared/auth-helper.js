edf3ae9b2aa79ae9429b517ceab691eec8cf59ab
Lars Jacobsson <lars.jacobsson@mathem.se>
2020-12-10 21:21:18 +0100

    upgrading version of aws-sdk-sso

--- src/commands/shared/auth-helper.js
+++ src/commands/shared/auth-helper.js
@@ -1,7 +1,6 @@
 const AWS = require("aws-sdk");
 require("@mhlabs/aws-sdk-sso");
 
-
 function initAuth(cmd) {
     process.env.AWS_PROFILE = cmd.profile || process.env.AWS_PROFILE || "default";
     process.env.AWS_REGION = cmd.region || process.env.AWS_REGION;
