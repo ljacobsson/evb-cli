430102e8db0f89ff760a42773a8fdce3d63b488b
ljacobsson <lars@mathem.se>
2022-11-23 21:23:50 +0100

    fix: profile bug when using credentials file

--- src/commands/shared/auth-helper.js
+++ src/commands/shared/auth-helper.js
@@ -2,15 +2,20 @@ const AWS = require("aws-sdk");
 require("@mhlabs/aws-sdk-sso");
 
 function initAuth(cmd) {
+
+  const credentials = new AWS.SharedIniFileCredentials({ profile: cmd.profile });
+  if (credentials.accessKeyId) {
+    AWS.config.credentials = credentials;
+  } else {
     process.env.AWS_PROFILE = cmd.profile || process.env.AWS_PROFILE || "default";
     process.env.AWS_REGION = cmd.region || process.env.AWS_REGION || AWS.config.region
     AWS.config.credentialProvider.providers.unshift(
       new AWS.SingleSignOnCredentials()
     );
   }
+}
 
-  module.exports = {
-      initAuth
-  }
+module.exports = {
+  initAuth
+}
 
-  
